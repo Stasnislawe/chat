3663fc1c31f26a57ac3605c6fe309bbe3b10e390
stas_ <dromanqs@mail.ru>
2024-12-04 21:55:33 +0500

    Добавление статики для отображения и загрузки изображений

--- project/project/settings.py
+++ project/project/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.sites',
+    'django.contrib.flatpages',
     'chat',
     'bootstrap3',
 ]
@@ -123,6 +125,9 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     BASE_DIR / "static"
 ]
+
+MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_URL = '/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
