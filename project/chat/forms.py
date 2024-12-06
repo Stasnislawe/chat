748ca46ed14c2fbd26fdfbcea00f24839e9a4c9f
stas_ <dromanqs@mail.ru>
2024-12-06 21:50:47 +0500

    Форма изменения профиля/создания

--- project/chat/forms.py
+++ project/chat/forms.py
@@ -1,8 +1,20 @@
 from django import forms
-from .models import Message
+from .models import Message, UserProfile
 
 
 class MessageForm(forms.ModelForm):
     class Meta:
         model = Message
         fields = ['message']
+
+
+class ProfileForm(forms.ModelForm):
+    class Meta:
+        model = UserProfile
+        fields = ['name',
+                  'image']
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['name'].widget.attrs['class'] = 'form-control'
+        self.fields['image'].widget.attrs['class'] = 'form-control'
