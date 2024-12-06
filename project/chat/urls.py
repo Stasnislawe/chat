09529a28f333651518b311ab607767eb514a05fb
stas_ <dromanqs@mail.ru>
2024-12-06 21:50:33 +0500

    Новая юрл изменения профия

--- project/chat/urls.py
+++ project/chat/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [
     path('dialogs/', views.DialogsView.as_view(), name='dialogs'),
     path('users/', views.Users.as_view(), name='users'),
+    path('dialogs/createuser', views.userprofile, name='create_user'),
     re_path(r'^dialogs/create/(?P<user_id>\d+)/$', views.CreateDialogView.as_view(), name='create_dialog'),
     re_path(r'^dialogs/(?P<chat_id>\d+)/$', views.MessagesView.as_view(), name='messages'),
 ]
