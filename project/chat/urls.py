e1af3c9c8947e00f0ff05d9cb646c4199602bce4
stas_ <dromanqs@mail.ru>
2024-12-15 21:57:03 +0500

    Удаление .views у классов/функций

--- project/chat/urls.py
+++ project/chat/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path, re_path
-from . import views
+from .views import DialogsView, Users, userprofile, CreateDialogView, MessagesView, createuser, CreateProfile
+
 
 urlpatterns = [
-    path('dialogs/', views.DialogsView.as_view(), name='dialogs'),
-    path('users/', views.Users.as_view(), name='users'),
-    path('dialogs/createuser', views.userprofile, name='create_user'),
-    re_path(r'^dialogs/create/(?P<user_id>\d+)/$', views.CreateDialogView.as_view(), name='create_dialog'),
-    re_path(r'^dialogs/(?P<chat_id>\d+)/$', views.MessagesView.as_view(), name='messages'),
+    path('dialogs/', DialogsView.as_view(), name='dialogs'),
+    path('users/', Users.as_view(), name='users'),
+    path('dialogs/createuser', userprofile, name='create_user'),
+    path('createuserprofile', CreateProfile.as_view(), name='create_profile'),
+    re_path(r'^dialogs/create/(?P<user_id>\d+)/$', CreateDialogView.as_view(), name='create_dialog'),
+    re_path(r'^dialogs/(?P<chat_id>\d+)/$', MessagesView.as_view(), name='messages'),
 ]
