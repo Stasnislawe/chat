d66d1105ee7dedfa434a344804b9aa804ae761d2
stas_ <dromanqs@mail.ru>
2024-12-15 21:56:03 +0500

    Url include sign.urls

--- project/project/urls.py
+++ project/project/urls.py
@@ -22,6 +22,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('chat.urls')),
+    path('accounts/', include('sign.urls')),
 ]
 
 if settings.DEBUG:
