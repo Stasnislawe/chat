6f5d779bb1469e9af3e0dbdf6dadac4b6b21f3fe
stas_ <dromanqs@mail.ru>
2024-12-15 20:36:05 +0500

    проверка has_changed

--- project/chat/forms.py
+++ project/chat/forms.py
@@ -9,12 +9,22 @@ class MessageForm(forms.ModelForm):
 
 
 class ProfileForm(forms.ModelForm):
+
+    def has_changed(self, *args, **kwargs):
+        changed_data = super().has_changed()
+        if not changed_data:
+            return False
+
+        relevant_fields = ['name', 'image']
+        for field in relevant_fields:
+            if field in changed_data:
+                return True
+
+        return False
+
     class Meta:
         model = UserProfile
         fields = ['name',
                   'image']
 
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.fields['name'].widget.attrs['class'] = 'form-control'
-        self.fields['image'].widget.attrs['class'] = 'form-control'
+
