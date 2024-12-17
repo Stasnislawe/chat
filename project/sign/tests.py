fe350744e6ef95ae24dbd061daf17b9008e97121
stas_ <dromanqs@mail.ru>
2024-12-17 16:30:38 +0500

    Изменение теста через Selenium, проверка статуса кода юрл, наследоваемых от LoginRequired

--- project/sign/tests.py
+++ project/sign/tests.py
@@ -1,6 +1,8 @@
 import time
+from django.test import TestCase
 
 from project.wsgi import *
+from django.test import Client
 from django.contrib.auth.models import User
 from django.test import LiveServerTestCase
 from django.urls import reverse
@@ -15,35 +17,62 @@ class MySeleniumTests(LiveServerTestCase):
     def setUpClass(cls):
         super().setUpClass()
         cls.service = Service(executable_path=GeckoDriverManager().install())
-        # cls.options = webdriver.FirefoxOptions()
-        # cls.options.add_argument("--headless")
-        cls.selenium = webdriver.Firefox(service=cls.service)
+        cls.options = webdriver.FirefoxOptions()
+        cls.options.add_argument("--headless")
+        cls.selenium = webdriver.Firefox(service=cls.service, options=cls.options)
 
+    """Регистрация с последующей авторизацией через селениум"""
     def test_login(self):
+        # Задаем юрл
         self.selenium.get(f'{self.live_server_url}/accounts/signup/')
+        username = 'Test1'
+        mail = '1@mail.ru'
+        password = 's4Dsa612H_21Zr'
+        # Селениум ищет и заполняет поля регистрации
         username_input = self.selenium.find_element("xpath", "//input[@id='id_username']")
-        username_input.send_keys('Test1')
+        username_input.send_keys(username)
         email_input = self.selenium.find_element("xpath", "//input[@id='id_email']")
-        email_input.send_keys('1@mail.ru')
+        email_input.send_keys(mail)
         password1 = self.selenium.find_element("xpath", "//input[@id='id_password1']")
-        password1.send_keys('s4Dsa612H_21Zr')
+        password1.send_keys(password)
         password2 = self.selenium.find_element("xpath", "//input[@id='id_password2']")
-        password2.send_keys('s4Dsa612H_21Zr')
+        password2.send_keys(password)
+        # Поиск кнопки "регистрация"
         registration_button = self.selenium.find_element("xpath", "//button[@id='regbutton']")
         registration_button.click()
-        time.sleep(2)
+        # Небольшая пауза для корректной работы
+        time.sleep(1)
+        # Авторизация по данным зарегестрированным секунду назад
         login_username = self.selenium.find_element("xpath", "//input[@id='id_username']")
-        login_username.send_keys('Test1')
+        login_username.send_keys(username)
         login_password = self.selenium.find_element("xpath", "//input[@id='id_password']")
-        login_password.send_keys('s4Dsa612H_21Zr')
+        login_password.send_keys(mail)
         login_button = self.selenium.find_element("xpath", "//button[@id='logbutton']")
         login_button.click()
 
-        self.object = User.objects.get(username='Test1')
-        self.assertEqual(str(self.object.username), 'Test1')
+        # Проверка что пользователь создан
+        self.object = User.objects.get(username=username)
+        self.assertEqual(str(self.object.username), username)
 
     @classmethod
     def tearDownClass(cls):
         cls.selenium.quit()
         super().tearDownClass()
 
+
+class LoginStatusCodeTest(TestCase):
+    def setUp(self):
+        self.username = 'user_test_1'
+        self.password = 'Tea3sdZ31S5X-1'
+        user = User.objects.create_user(username=self.username, email='1@mail.ru', password=self.password)
+
+    def test_status_code_logined(self):
+        c = Client()
+        c.login(username=self.username, password=self.password)
+        response_dialogs = c.get(reverse('dialogs'))
+        self.assertEqual(response_dialogs.status_code, 200)
+        response_users = c.get(reverse('users'))
+        self.assertEqual(response_users.status_code, 200)
+
+    def tearDown(self):
+        pass
